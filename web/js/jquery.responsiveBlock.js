b09aafe7661e947e7d603d20709a70e359de102d
Alexey Cherevko <you@example.com>
2019-09-06 16:03:34 +0700

    13-06 0609 Dalneysheja realizazia

--- web/js/jquery.responsiveBlock.js
+++ web/js/jquery.responsiveBlock.js
@@ -1,11 +1,25 @@
-
 (function($){
-  jQuery.fn.responsiveBlock = function(){
+  jQuery.fn.responsiveBlock = function(options){
+    options = $.extend({
+      defColor:"white", //цвет элемента над которым нет курсора
+      hoverColor:"red" //цвет элемента на который наведен курсор
+    }, options);
+
     var make = function(){
-      // реализация работы метода с отдельным элементом страницы
+      // здесь переменная this будет содержать отдельный
+      // DOM-элемент, к которому и нужно будет применить
+      // воздействия плагина
+      $(this).css("background-color",options.defColor)
+      .mouseenter( function(){
+        $(this).css("background-color",options.hoverColor);
+      })
+      .mouseleave( function(){
+        $(this).css("background-color",options.defColor);
+      });
     };
- 
+
     return this.each(make); 
-    // в итоге, метод responsiveBlock вернет текущий объект jQuery обратно
   };
 })(jQuery);
+
+$('div').responsiveBlock();
